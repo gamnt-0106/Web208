fb93fff46734d8c915e3d20ceea49e6d0970427a
gamnt-0106 <gamntph14216@fpt.edu.vn>
2022-06-15 20:38:21 +0700

    product,detail,sigin,sigup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { BlogComponent } from './blog/blog.component';
